7fc99087a041058b3534e534e700c430d76de15a
Shashiwadana Nirmani <shashiwadana_n@3logixs.lk>
2019-10-07 10:24:13 +0530

    created database

--- src/server/index.js
+++ src/server/index.js
@@ -15,6 +15,42 @@ const app = express();
 app.use(cors());
 app.use(express.static("public"));
 
+//var express = require('express');
+var mysql = require('mysql');
+
+var connection =mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'',
+  database:'intern_test'
+});
+connection.connect(function(error){
+  if(!!error){
+    console.log('error connection');
+  }
+  else{
+    console.log('connected to database');
+    
+  }
+});
+
+app.use(function(req,res,next){
+  req.connection=connection;
+  next();
+});
+
+/*
+app.get('/about',function(req,res){
+  connection.query("SELECT * FROM User",function(error,rows,feilds){
+  if(!!error){
+    console.log('Error query');
+  }
+  else{
+    console.log('success query');
+    console.log(rows);
+  }
+})
+});*/
 
 
 app.get("*", (req, res, next) => {
